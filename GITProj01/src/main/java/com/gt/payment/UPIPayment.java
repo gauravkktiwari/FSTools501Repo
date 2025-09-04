3d927bd56640e91a9ec215c421c8b04aad3e48b9
'Gaurav <gaurav.tiwari@oneway.cab>
2025-09-04 23:17:18 +0530

    dev2 -- commit

--- GITProj01/src/main/java/com/gt/payment/UPIPayment.java
+++ GITProj01/src/main/java/com/gt/payment/UPIPayment.java
@@ -3,6 +3,10 @@ package com.gt.payment;
 public class UPIPayment {
 
 	public String doPymentUsingPhonePe(long phoneno, double amount) {
-		return "payment is happening from the phone number: "+phoneno;
+		return "payment is happening from the phone number: "+phoneno+" using phonepe";
+	}
+	
+	public String doPymentUsingGpay(long phoneno, double amount) {
+		return "payment is happening from the phone number: "+phoneno+" using gpay";
 	}
 }
