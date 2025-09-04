b0c6ffc0c6182916a1478e3241333435696f7a46
'Gaurav <gaurav.tiwari@oneway.cab>
2025-09-04 23:12:45 +0530

    Dev1 performing UPI Payment

---
+++ GITProj01/src/main/java/com/gt/payment/UPIPayment.java
@@ -0,0 +1,8 @@
+package com.gt.payment;
+
+public class UPIPayment {
+
+	public String doPymentUsingPhonePe(long phoneno, double amount) {
+		return "payment is happening from the phone number: "+phoneno;
+	}
+}
